2b3fa2be252de0ebf2cb05a262591df3c05b8386
Pavel Ruban <pasharuban211425@mail.ru>
2021-11-29 00:38:37 +0300

    installed react router + added basic paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import MainPage from './pages/MainPage/MainPage';
 import TasksTablePage from './pages/TasksTablePage/TasksTablePage';
-
-/* import MainPage from './pages/MainPage/MainPage'; */
+import NewClaimForm from './pages/TasksTablePage/components/NewClaim/NewClaimForm';
+import IncomingClaimForm from './pages/TasksTablePage/components/IncomingClaim/IncomingClaimForm';
 
 function App() {
-  return <TasksTablePage />;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <MainPage />
+        </Route>
+        <Route path="/dashboard">
+          <TasksTablePage />
+        </Route>
+        <Route path="/create">
+          <NewClaimForm />
+        </Route>
+        <Route path="/edit/:id">
+          <IncomingClaimForm />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
